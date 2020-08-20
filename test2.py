42e1ecddda12546547f6cc87102ddefd87f52387
Peter Voorhees <peter@peters-air.lan>
2020-08-19 20:41:06 -0400

    second test commit

---
+++ test2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 20:36:37 2020
+
+@author: peter
+"""
+
+#this is a second test file for github
+
+for i in range(10):
+    print("poop")
